abd45bc698572574f30d294a026a26b1bb5d9c00
mrsolusdev <btemirkhodjaev@gmail.com>
2026-07-03 17:52:22 +0500

    feat: scaffold veyron-wire crate with WireError and proto codegen

---
+++ wire/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod error;
+pub mod proto {
+    #![allow(clippy::enum_variant_names)]
+    pub mod veyron {
+        include!(concat!(env!("OUT_DIR"), "/veyron.rs"));
+    }
+}
+
+pub use error::WireError;
